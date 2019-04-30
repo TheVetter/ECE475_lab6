58268a8449d76489ad625760878ba55832477282
SidneyStone <sidneylstone@gmail.com>
2019-04-30 11:49:24 -0500

    Update MULT.vhd

--- MULT.vhd
+++ MULT.vhd
@@ -14,7 +14,67 @@ entity MULT is
 
 end MULT;
 
+entity AND2 is
+	generic(width : positive := 2);
+	port(A: in std_logic_vector(width-1 downto 0);
+		Z: out std_logic);
+end;
+architecture behav of AND2 is
+begin
+	p1: process(A)
+	variable temp: std_logic := '1';
+	begin
+		for i in A'length-1 downto 0 loop
+			temp := temp and A(i);
+		end loop;
+		Z <= temp;
+	end process;
+end architecture;
+
+entity NAND2 is	
+	generic(width: positive := 2);
+	port(A : in std_logic_vector(width-1 downto 0)
+		Z: out std_logic);
+end;
+
+architecture behav of NAND2 is
+begin
+	p1: process(A)
+	begin
+		Z <= not(AND2(A));
+	end process;
+end;
+
+entity HA is
+	generic()
+	port()
+end;
+architecture behav of HA is
+begin
+
+end;
+
+entity FA is
+	generic()
+	port()
+end;
+architecture behav of FA is
+begin
+
+end;
+
+entity RCA is 
+	generic()
+	port()
+end;
+
+architecture behav of RCA is
+begin
+
+end;
+	
 architecture struct of MULT is 
+
 begin
 
 end struct; 
