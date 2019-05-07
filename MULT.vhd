1e47b6e0f987b2eaaa9421a2a3939a080468e025
MFrensko <38331440+MFrensko@users.noreply.github.com>
2019-05-07 11:35:37 -0500

    Fixed partial product generation

--- MULT.vhd
+++ MULT.vhd
@@ -175,15 +175,15 @@ architecture struct of MULT is
 
 	
 	--initializing arrays
-	type PP is array(Y_Len - 1 downto 0, X_Len - 1 downto 0) of std_logic;
+	type PP is array(0 to Y_Len, 0 to X_Len - 1) of std_logic;
 		signal inter_product : PP; 									--Y rows, X columns
-	type Sum_array is array(Y_Len downto 0, X_Len downto 0) of std_logic;
+	type Sum_array is array(0 to Y_Len, 0 to X_Len) of std_logic;
 		signal inter_sum: Sum_array;   
-	type Carry_array is array(Y_Len downto 0, X_Len - 1 downto 0) of std_logic;
+	type Carry_array is array(0 to Y_Len, 0 to X_Len - 1) of std_logic;
 		signal inter_carry: Carry_array;
 
-	signal tempInterArray : arrayType(Y_Len downto 0, X_Len downto 0);
-	signal tempCarryArray : arrayType(Y_Len downto 0, X_Len - 1 downto 0);
+	signal tempInterArray : arrayType(0 to Y_Len, 0 to X_Len );
+	signal tempCarryArray : arrayType(0 to Y_Len, 0 to X_Len - 1);
 
 	signal result : std_logic_vector(X_Len+Y_Len downto 0) := (others => '0');
 	signal carryo : std_logic;
@@ -191,53 +191,53 @@ architecture struct of MULT is
 begin
 		
 	--generate 2d array of partial products
-	ppgen1: for i in Y_Len-1 downto 0 generate
-		ppgen2: for j in X_Len-1 downto 0 generate
+	ppgen1: for i in 0 to Y_Len generate
+		ppgen2: for j in 0 to  X_Len-1 generate
 		
-			PP_AND: if not(i /= Y_Len-1 xor j /= X_Len-1) generate
+			PP_AND: if not(i /= Y_Len xor j /= X_Len-1) generate
 				bob: myAND port map(Y(i),X(j),inter_product(i,j));
 			end generate;
 			
-			PP_NAND: if (i /= Y_Len-1 xor j /= X_Len-1) generate
+			PP_NAND: if (i /= Y_Len xor j /= X_Len-1) generate
 				banana: myNAND port map(Y(i),X(j),inter_product(i,j));
 			end generate;
 			
 		end generate;
 	end generate;
 	
-	--Half adder for loop
-	hagen1: for j in 1 to X_Len-1 generate
-		pineapple: HA port map(inter_product(0, j), inter_product(1, j-1), inter_sum(1, j), inter_carry(1, j));
-	end generate;
+	-- --Half adder for loop
+	-- hagen1: for j in 1 to X_Len-1 generate
+		-- pineapple: HA port map(inter_product(0, j), inter_product(1, j-1), inter_sum(1, j), inter_carry(1, j));
+	-- end generate;
 
-	--Middle rows for loop
-		--inside FA loop
-	mgen1: for i in 2 to Y_Len-1 generate
-		mgen2: for j in 0 to X_Len-2 generate
-			mango: FA port map(inter_product(i, j), inter_sum(i-1, j+1), inter_carry(i-1,j+1), inter_sum(i,j), inter_carry(i,j+1)) ;
-		end generate;
+	-- --Middle rows for loop
+		-- --inside FA loop
+	-- mgen1: for i in 2 to Y_Len-1 generate
+		-- mgen2: for j in 0 to X_Len-2 generate
+			-- mango: FA port map(inter_product(i, j), inter_sum(i-1, j+1), inter_carry(i-1,j+1), inter_sum(i,j), inter_carry(i,j+1)) ;
+		-- end generate;
 		
-		apple: FA port map(inter_product(i, X_Len-2), inter_product(i-1, X_Len-1), inter_carry(i-1, X_Len-1), inter_sum(i, X_Len-1), inter_carry(i, X_Len-1)) ;
-	end generate;
+		-- apple: FA port map(inter_product(i, X_Len-2), inter_product(i-1, X_Len-1), inter_carry(i-1, X_Len-1), inter_sum(i, X_Len-1), inter_carry(i, X_Len-1)) ;
+	-- end generate;
 
 
-	--last row for loop
-		-- lonely full adder (half adder whose carry-in is always 1)
-	durian: FA port map (inter_sum(y_len-1, 1), inter_carry(Y_Len-1, 1), '1' , inter_sum(Y_Len, 0), inter_carry(Y_Len, 0));
-		--inside FA loop
-	finalgen: for i in 1 to X_Len-2 generate
-		grape: FA port map (inter_sum(y_len-1, i), inter_carry(Y_Len-1, i),inter_carry(Y_Len, i), inter_sum(Y_Len, i), inter_carry(Y_Len, i+1));
-	end generate;
+	-- --last row for loop
+		-- -- lonely full adder (half adder whose carry-in is always 1)
+	-- durian: FA port map (inter_sum(y_len-1, 1), inter_carry(Y_Len-1, 1), '1' , inter_sum(Y_Len, 0), inter_carry(Y_Len, 0));
+		-- --inside FA loop
+	-- finalgen: for i in 1 to X_Len-2 generate
+		-- grape: FA port map (inter_sum(y_len-1, i), inter_carry(Y_Len-1, i),inter_carry(Y_Len, i), inter_sum(Y_Len, i), inter_carry(Y_Len, i+1));
+	-- end generate;
 
-	-- lonely full adder (whose carry-out is special (bottom left most FA))
-	cherry: FA port map (inter_product(y_len-1, x_len-1), inter_carry(Y_Len-1, 1), inter_carry(Y_Len, x_len-1) , inter_sum(Y_Len, X_Len-1), inter_carry(Y_Len, x_len-1	));
+	-- -- lonely full adder (whose carry-out is special (bottom left most FA))
+	-- cherry: FA port map (inter_product(y_len-1, x_len-1), inter_carry(Y_Len-1, 1), inter_carry(Y_Len, x_len-1) , inter_sum(Y_Len, X_Len-1), inter_carry(Y_Len, x_len-1	));
 	
 	
-	tempInterArray <= arrayType(inter_sum);
-	tempCarryArray <= arrayType(inter_carry);
-	--get the result
-	hspuck : hockeystick --generic map(X_Len, Y_Len)
-				port map(tempInterArray, tempCarryArray, result);
+	-- tempInterArray <= arrayType(inter_sum);
+	-- tempCarryArray <= arrayType(inter_carry);
+	-- --get the result
+	-- hspuck : hockeystick --generic map(X_Len, Y_Len)
+				-- port map(tempInterArray, tempCarryArray, result);
 
 	-- addOnes: for i in Y_Len to X_Len+Y_Len generate
 		-- papple: if (i = Y_Len) generate
@@ -251,6 +251,6 @@ begin
 		-- end generate;
 	-- end generate;
 
-	P <= inter_sum(4,4) & inter_sum(4,3) & inter_sum(4,2) & inter_sum(4,1) & inter_sum(4,0) & inter_sum(3,0) &inter_sum(2,0) &inter_sum(1,0) & inter_sum(0,0);
+	P <= inter_product(4,3)&inter_product(4,2)&inter_product(4,1)&inter_product(4,0)&inter_product(3,3)&inter_product(2,3)&inter_product(1,3)&inter_product(0,3)&inter_product(3,2);
 
 	end struct;
